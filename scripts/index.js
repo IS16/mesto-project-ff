75b6bd9fc1724340825f40f88057415b5068a75f
IS16 <sarkisov775@yandex.ru>
2024-10-06 05:31:16 +0300

    Вторая итерация: изменено название функции, определена переменная для удаления повторного поиска элемента

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const addCard = (name = '', link = '', deleteCardCallback = deleteCard) => {
+const createCard = (name = '', link = '', deleteCardCallback = deleteCard) => {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__image').src = link;
-    cardElement.querySelector('.card__image').alt = name;
+    const cardImage = cardElement.querySelector('.card__image');
+
+    cardImage.src = link;
+    cardImage.alt = name;
     cardElement.querySelector('.card__title').textContent = name;
 
     cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCardCallback);
@@ -23,4 +25,4 @@ const deleteCard = (elem) => {
 };
 
 // @todo: Вывести карточки на страницу
-placesList.append(...initialCards.map(item => addCard(item.name, item.link, deleteCard)));
+placesList.append(...initialCards.map(item => createCard(item.name, item.link, deleteCard)));
